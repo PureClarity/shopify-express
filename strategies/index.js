70f2d1fb096faf974955807d90d5d58c18c1bef3
Peter Brooksbank <peter.brooksbank@pureclarity.com>
2019-09-02 13:56:27 +0100

    Removed SQL strategy

--- strategies/index.js
+++ strategies/index.js
@@ -1,9 +1,6 @@
 const RedisStrategy = require('./RedisStrategy');
 const MemoryStrategy = require('./MemoryStrategy');
-const SQLStrategy = require('./SQLStrategy');
-
 module.exports = {
   RedisStrategy,
   MemoryStrategy,
-  SQLStrategy,
 }
